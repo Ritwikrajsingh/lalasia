1e8a01d2c873a09d24e4f3a5c6f95c9d9194b7ed
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2023-11-19 15:38:43 +0530

    created a dummy database file

---
+++ src/database/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    footer: [
+        ["Product", "New Arrivals", "Best Selling", "Home Decor", "Kitchen Set"],
+        ["Services", "Catalog", "Blog", "FaQ", "Pricing"],
+        ["Follow Us", "Facebook", "Instagram", "Twitter"],
+    ]
+}
