561a4617991aa3392960ed60b1d6b5eb8d7843cd
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2023-11-20 15:40:27 +0530

    used Header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
 import './styles/App.css';
 
 function App() {
-  return (
-    <a
+  return (<>
+    <Header />
+    {/* <a
       className="App-link"
       href="#"
       // target="_blank"
       rel="noopener noreferrer"
     >
       Hello World!
-    </a>
+    </a> */}
+  </>
   );
 }
 
