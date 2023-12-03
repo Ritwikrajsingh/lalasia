250d7fbb4e72b9a41fe2cb6fb97d7192cca2be2c
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2023-12-03 13:21:52 +0530

    used hamburger on App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import Footer from './components/Footer';
-import Header from './components/Header';
-import './styles/App.css';
+import { useState } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import ResponsiveNav from "./components/Header/responsive";
+import "./styles/App.css";
 
 function App() {
-  return (<>
-    <Header />
-    {/* <a
+  const [hamburgerMenu, setHamburgerMenu] = useState(false);
+  console.log(hamburgerMenu, "value of hamburgerMenu");
+  return (
+    <>
+      <Header
+        hamburgerMenu={hamburgerMenu}
+        setHamburgerMenu={setHamburgerMenu}
+      />
+      {/* <a
       className="App-link"
       href="#"
       // target="_blank"
@@ -13,11 +21,12 @@ function App() {
     >
       Hello World!
     </a> */}
-    <main style={{minHeight:"50vh", margin: "10% 0% -13% 40%"}}>
-      <h1>What the hell is happening here</h1>
-    </main>
-    <Footer />
-  </>
+      <main style={{ minHeight: "50vh" }}>
+        <ResponsiveNav hamburgerMenu={hamburgerMenu} />
+        <h1>What the hell is happening here</h1>
+      </main>
+      <Footer />
+    </>
   );
 }
 
